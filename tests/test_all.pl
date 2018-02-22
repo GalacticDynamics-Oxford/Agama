3c2553df6ea9e6cda415e5ab45da0c0226abf79d
Eugene <eugvas@lpi.ru>
2018-02-22 23:45:11 +0000

    Public release

--- tests/test_all.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-
-$ok=1;
-while(<*.exe>) {
-    print "$_  ";
-    $result=`./$_`;
-    if($result=~/ALL TESTS PASSED/) { print "OK\n"; } else { print "FAILED\n"; $ok=0; }
-}
-if($ok) { print "ALL TESTS PASSED\n"; }
